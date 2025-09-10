03e21ba78fcd7d62ccaabe1feafefa166dab212a
Parxer <jakub.freywald@gmail.com>
2025-09-10 21:30:07 +0200

    set up chakra-ui, clean up old styles

---
+++ app/providers.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+import {ChakraProvider, defaultSystem } from "@chakra-ui/react";
+import {ReactNode} from "react";
+
+export default function Providers({ children }: { children: ReactNode }) {
+    return (
+        <ChakraProvider value={defaultSystem}>{children}</ChakraProvider>
+    );
+}
